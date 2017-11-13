a116909940bd409de75a42bd353be9526da5e0a6
MockingT <31817173+MockingT@users.noreply.github.com>
2017-11-13 17:14:12 +0800

    Update tree.h

--- tree.h
+++ tree.h
@@ -20,8 +20,15 @@ class TreeMethod
 {
 public:
     TreeNode *root;
-    void build();
-    void printTheTree();
+	TreeMethod();
+	void creat_binary_tree(TreeNode *&root);
+	void show_binary_tree(TreeNode *&root,int i);
+	void search_all_information1(string num,TreeNode *&root);
+	void search_all_information2(string name,TreeNode *&root);
+	void search_all_height1(string num,TreeNode *&root);
+	void search_all_height2(string name,TreeNode *&root);
+	void add_child(string num,TreeNode *&root);
+	int is_rename(string name,TreeNode *&root,int i);
 }tree;
 
 
