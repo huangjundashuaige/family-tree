d6fe20fa5e133e5b7fd42f8eccb105bc21b65bf3
MockingT <31817173+MockingT@users.noreply.github.com>
2017-11-14 00:47:33 +0800

    Update tree.h

--- tree.h
+++ tree.h
@@ -1,32 +1,36 @@
 #ifndef TREE_HEADFILE
 #define TREE_HEADFILE
 
-#include <string>
-#include <vector>
+#include<string>
+#include <iostream>
 using namespace std;
 
 
 struct binaryTreeNode
 {
-	string person_num;  
-	string person_name; 
-	string person_sex;  
-	string partner_name; 
-	string partner_sex;  
-	binaryTreeNode *first_child,*next_sibling,*front_parent,*front_sibling;  
-	
+public:
+	string person_num;
+	string person_name;
+	string person_sex;
+	string partner_name;
+	string partner_sex;
+	binaryTreeNode *first_child,*next_sibling,*front_parent,*front_sibling;
 };
 
-class binaryTree {
+class binaryTree
+{
 public:
 	binaryTreeNode *root;
+public:
 	binaryTree();
 	void creat_binary_tree(binaryTreeNode *&root);
-	void show_binary_tree(binaryTreeNode *&root, int i);
-	void search_all_information1(string num, binaryTreeNode *&root);
-	void search_all_information2(string name, binaryTreeNode *&root);
-	void add_child(string num, binaryTreeNode *&root);
-	int is_rename(string name, binaryTreeNode *&root, int i);
-}
+	void show_binary_tree(binaryTreeNode *&root,int i);
+	void search_all_information1(string num,binaryTreeNode *&root);
+	void search_all_information2(string name,binaryTreeNode *&root);
+	void add_child(string num,binaryTreeNode *&root);
+	int is_rename(string name,binaryTreeNode *&root,int i);
+
+};
+
 
 #endif
